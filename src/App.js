bfab0a4361a32df4bc0115218045ebe6096c7834
Marcela Felix Fortis <marmarmarcelacela@gmail.com>
2021-06-17 13:14:13 +0200

    about me section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './sass/main.scss';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
-
-
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Header/>
+      <About/>
     </div>
   );
 }
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,10 +1,31 @@
 import React from "react";
-import data from "./data";
 
-export default function About(data) {
+import data from "./data";
+import  me  from "../assets/img/about.png";
+
+
+export default function About() {
+
+    const {aboutme} = data;
+
     return(
         <section className="about">
-
+            <div className="row">
+            <div className="col-1-of-2">
+                <h1 className="heading-primary--main">
+                <div class="u-align-center u-margin-bottom-big">    
+                    About me
+                </div>
+                    </h1>
+                <p className="paragraph">
+                    {aboutme}
+                </p>
+            </div>
+            <div className="col-1-of-2">
+                    <img src= { me } alt="woman" 
+                    className="about__image" />
+            </div>
+            </div>
         </section>
     )
 }
