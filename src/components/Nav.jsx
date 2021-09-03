4657e006bcd7da9513893bef9b77913e5e1d4611
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-09-03 21:09:45 +0200

    final styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,13 +9,19 @@ export default function Nav() {
 	return (
 		<nav className="nav">
 			<ul className="nav-list">
+				<div className="desktop-logo">
+					<a href="#hero">
+						<img src={Logo} alt="Letters MF" />
+					</a>
+					<div className="owner"> <h3>Marcela Felix Fortis</h3></div>
+				</div>
 				<li>
 					<a href="#about">About</a>
 				</li>
 				<li>
 					<a href="#projects">Projects</a>
 				</li>
-				<li>
+				<li className="mobile-logo">
 					<a href="#hero">
 						<img src={Logo} alt="Letters MF" />
 					</a>
